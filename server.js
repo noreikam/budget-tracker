1c4982e0537160d975e5696b3a52e3fa7a2cba5d
noreikam <noreikam1@gmail.com>
2022-06-02 21:15:53 -0400

    try 1 fix mongoose.connect

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget-tracker', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
 
 const app = express();
 
@@ -19,10 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget-tracker', {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
+
+mongoose.set('debug', true);
 
 // routes
 app.use(require("./routes/api.js"));
